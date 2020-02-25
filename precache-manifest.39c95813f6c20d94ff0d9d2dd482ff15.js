66f4d0ad29a1924b7b742b4ed0b121caaaa9aac7
Matheus Oliveira <mattoliveirabsb@gmail.com>
2020-02-24 19:35:48 -0700

    Updates

---
+++ precache-manifest.39c95813f6c20d94ff0d9d2dd482ff15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b23e839333eadd1d439ce8552fead0",
+    "url": "/codechallenge-frontend/index.html"
+  },
+  {
+    "revision": "36949f455889bd3694b5",
+    "url": "/codechallenge-frontend/static/js/2.8e44b425.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/codechallenge-frontend/static/js/2.8e44b425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f3395647625956a022",
+    "url": "/codechallenge-frontend/static/js/main.c9446e39.chunk.js"
+  },
+  {
+    "revision": "bd98d43a1b4e60d09ae0",
+    "url": "/codechallenge-frontend/static/js/runtime-main.623f1e5c.js"
+  }
+]);
